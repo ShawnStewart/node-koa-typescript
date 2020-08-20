dea576a22a3a166b276c301da1c0c7a8495c2091
Shawn Stewart <s.stewart0220@gmail.com>
2020-08-20 09:36:50 -0500

    Updated seeds

--- src/database/seeds/0001_users.ts
+++ src/database/seeds/0001_users.ts
@@ -6,26 +6,10 @@ export async function seed(knex: Knex): Promise<void> {
 
     return await knex('users').insert([
         {
-            username: 'shawn',
-            email: 'shawn@email.com',
+            username: 'username',
+            email: 'user.name@email.com',
             password: '$2a$11$3iVnQ2JpO7QL4zr9zu69/uE78RQVIMBl.kGfrOP5wIlKsqz.a2YW.',
             isDeleted: false,
-        },
-        {
-            username: 'alec',
-            password: '$2a$11$3iVnQ2JpO7QL4zr9zu69/uE78RQVIMBl.kGfrOP5wIlKsqz.a2YW.',
-            isDeleted: false,
-        },
-        {
-            username: 'xavier',
-            password: '$2a$11$3iVnQ2JpO7QL4zr9zu69/uE78RQVIMBl.kGfrOP5wIlKsqz.a2YW.',
-            isDeleted: false,
-        },
-        {
-            username: 'jake',
-            password: '$2a$11$3iVnQ2JpO7QL4zr9zu69/uE78RQVIMBl.kGfrOP5wIlKsqz.a2YW.',
-            deletedAt: new Date(),
-            isDeleted: true,
         },
     ]);
 }
